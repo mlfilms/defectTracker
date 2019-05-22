2753d4af1a2ed6a3ee71f246aa755b5b80db8b19
Eric Minor <ermi1253@colorado.edu>
2019-05-21 19:55:19 -0600

    updated model code

--- darkflow/runFlow.py
+++ darkflow/runFlow.py
@@ -1,15 +1,82 @@
 from darkflow.net.build import TFNet
 import cv2
+import matplotlib.pyplot as plt
+import pprint as pp
+import numpy as np
+from PIL import Image
+import glob
+import os
+import json
 
-options = {"model": "cfg/yolo_custom.cfg", 
-           "load": "bin/yolo.weights",
-           "batch": 8,
-           "epoch": 100,
+def boxing(original_img , predictions):
+    newImage = np.copy(original_img)
+
+    for result in predictions:
+        top_x = result['topleft']['x']
+        top_y = result['topleft']['y']
+
+        btm_x = result['bottomright']['x']
+        btm_y = result['bottomright']['y']
+    
+        confidence = result['confidence']
+        label = result['label'] + " " + str(round(confidence, 3))
+        
+        if confidence > 0.3:
+            newImage = cv2.rectangle(newImage, (top_x, top_y), (btm_x, btm_y), (255,0,0), 3)
+            #newImage = cv2.putText(newImage, label, (top_x, top_y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL , 0.8, (0, 230, 0), 1, cv2.LINE_AA)
+        
+    return newImage
+	
+	
+	
+	
+def processImage(filename, tfnet):
+    imgcv = cv2.imread(filename)
+    result = tfnet.return_predict(imgcv)
+    #print(result)
+    newImage = boxing(imgcv, result)
+    im = Image.fromarray(newImage)
+    return (im,result)
+    #im.save("your_file.jpeg")
+
+
+options = {"model": "cfg/yolo_custom2.cfg", 
+           "load": -1,
            "gpu": 1.0,
-           "train": True,
-           "annotation": "C:/Users/Eric/Downloads/soccer_ball_data/annotations/",
-		   "labels": "ball_label.txt",
-           "dataset": "C:/Users/Eric/Downloads/soccer_ball_data/images/"}
-           
+		   "threshold": 0.1,
+		   "labels": "one_label.txt"
+		   }
 tfnet = TFNet(options)
-tfnet.train()
+targetDir = "E:\Projects\defectTracker\images"
+
+outDir = targetDir+"\out\\"
+if not os.path.exists(outDir):
+    os.makedirs(outDir)
+		   
+for filename in glob.glob(targetDir+"\*.jpg"):
+    (im,result) = processImage(filename,tfnet);
+    sections = filename.split("\\")
+    imName = sections[-1]
+    im.save(outDir+imName)
+    dataJSON = json.dumps(str(result))
+    prePost = imName.split(".")
+    noEnd = prePost[0]
+    
+    f = open(outDir+noEnd+".json","w")
+    f.write(dataJSON)
+    f.close
+    
+           
+
+#imgcv = cv2.imread("E:\Projects\defectTracker\images\circle_1.jpg")
+
+
+#print("something")
+#wait = input("PRESS ENTER TO CONTINUE.")
+#print("something")
+
+
+
+
+
+
