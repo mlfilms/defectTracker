e9c66dde4d9243b1b25080f4812b4c4751a84b65
ericminor370 <ericminor370@gmail.com>
2019-05-23 11:36:36 -0600

    cleanup

--- anTester.py
+++
@@ -1,8 +0,0 @@
-imgPath = 'C:\Users\Eric Minor\TrackingML\defectTracker\images\circle_1.jpg'
-xmlfilename = 'circle_1'
-
-bboxList = []
-im = Image.open(imgPath)
-bboxList.append((10, 10, 20, 20))
-
-createXMLAnnotation(imgPath, bboxList, im.size, self.xmlfilename)
